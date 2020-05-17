ff18ff98c8b1c82312cb38aa6346b71105887278
Hwang <justin@goliath.org>
2020-05-17 16:31:48 -0700

    Created new file

---
+++ src/zombiesurvivalgame/GameOverScreen.java
@@ -0,0 +1,42 @@
+package zombiesurvivalgame;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class GameOverScreen implements MouseMotionListener, MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
