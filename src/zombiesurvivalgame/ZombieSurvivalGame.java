fef61df1b96890dd6ae003d5d61635f8c460aee3
Hwang <justin@goliath.org>
2020-05-19 10:00:44 -0700

    Player armor level now resets on new game, as intended

--- src/zombiesurvivalgame/ZombieSurvivalGame.java
+++ src/zombiesurvivalgame/ZombieSurvivalGame.java
@@ -100,6 +100,7 @@ public class ZombieSurvivalGame extends JPanel {
         player.setMaxHealth(100.0);
         player.setHealth(100.0);
         player.setGun(new Pistol((int) player.getX(), (int) player.getY(), this));
+        player.setArmorLevel(0);
         monsterCount = 2;
 
         seconds = 0;
