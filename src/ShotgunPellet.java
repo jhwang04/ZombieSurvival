9adb5bf308d7379a5311f5d41e6588419ad62e8c
jhwang04 <justin@goliath.org>
2020-05-23 15:39:17 -0700

    Renamed variables to be appropriate for shotgun

--- src/ShotgunPellet.java
+++ src/ShotgunPellet.java
@@ -1,14 +1,21 @@
 import java.awt.*;
 
+/*
+Rishi Zamvar
+5/20/2020
+Rev: 02
+Notes: Changed values to work for shotgun
+ */
+
 public class ShotgunPellet extends Projectile {
-    public static final double PISTOL_BULLET_SPEED = 10.0; //placeholder value, is arbitrary
-    public static final double PISTOL_BULLET_DAMAGE = 20;
-    public static final int PISTOL_RANGE = 250;
-    public static final int PISTOL_BULLET_WIDTH = 20;
-    public static final int PISTOL_BULLET_HEIGHT = 20;
-    public static final Image PISTOL_BULLET_IMAGE = null;
+    public static final double SHOTGUN_BULLET_SPEED = 10.0; //placeholder value, is arbitrary
+    public static final double SHOTGUN_BULLET_DAMAGE = 20;
+    public static final int SHOTGUN_RANGE = 250;
+    public static final int SHOTGUN_BULLET_WIDTH = 20;
+    public static final int SHOTGUN_BULLET_HEIGHT = 20;
+    public static final Image SHOTGUN_BULLET_IMAGE = null;
 
     public ShotgunPellet(int x, int y, double trajectory, ZombieSurvivalGame game) {
-        super(x, y, PISTOL_BULLET_SPEED, PISTOL_BULLET_DAMAGE, PISTOL_RANGE, PISTOL_BULLET_IMAGE, trajectory, game, x - PISTOL_BULLET_WIDTH/2, y - PISTOL_BULLET_HEIGHT/2, PISTOL_BULLET_WIDTH, PISTOL_BULLET_HEIGHT);
+        super(x, y, SHOTGUN_BULLET_SPEED, SHOTGUN_BULLET_DAMAGE, SHOTGUN_RANGE, SHOTGUN_BULLET_IMAGE, trajectory, game, x - SHOTGUN_BULLET_WIDTH/2, y - SHOTGUN_BULLET_HEIGHT/2, SHOTGUN_BULLET_WIDTH, SHOTGUN_BULLET_HEIGHT);
     }
 }
